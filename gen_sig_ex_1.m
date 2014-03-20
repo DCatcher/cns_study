9602e072a001d05cf4b30a4e6c02ffa4b8f3d13a
zcx <nbfhzcx002@163.com>
2014-03-20 21:38:04 +0800

    Fast!

--- gen_sig_ex_1.m
+++ gen_sig_ex_1.m
@@ -20,8 +20,8 @@ function [sig_ex_all, sig_in_all] = gen_sig_ex_1(n,m,tmax,fs,lamda);
             pre = [pre pre_all(t_left,:)+pre(end)];
             t_left = t_left+1;
         end
-        
-        [tmp ans_lists] = findpeaks((pre<tn)*2+(1:length(pre)));
+        ans_lists = find((pre>tn),1);
+%         [tmp ans_lists] = findpeaks((pre<tn)*2+(1:length(pre)));
         ans_my = round(pre(1:ans_lists(1)))+1;        
         sig_ex_all(ans_my,i) = 1;
 	end
@@ -31,7 +31,8 @@ function [sig_ex_all, sig_in_all] = gen_sig_ex_1(n,m,tmax,fs,lamda);
             pre = [pre pre_all(t_left,:)+pre(end)];
             t_left = t_left+1;
         end        
-        [tmp ans_lists] = findpeaks((pre<tn)*2+(1:length(pre)));        
+        ans_lists = find((pre>tn),1);
+%         [tmp ans_lists] = findpeaks((pre<tn)*2+(1:length(pre)));        
         ans_my = round(pre(1:ans_lists(1)))+1;          
         sig_in_all(ans_my,i) = 1;
 	end
